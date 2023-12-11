966101f1d81fade27969c9085312cb6ce2d43926
Dmytro Zl <zl.dmitriy94@gmail.com>
2023-12-11 22:05:43 +0200

    Task 4 finished, HW2 completed

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,30 @@
-// function getShippingMessage(country, price, deliveryFee) {
-//     let totalPrice = price + deliveryFee;
-//     let message = `Shipping to ${country} will cost ${totalPrice} credits`;
-//     return message
-// }
+function getShippingCost(country) {
+    switch (country) {
+        case 'China':
+            price = 100;
+            message = `Shipping to ${country} will cost ${price} credits`;
+            break;
+        case 'Chile':
+            price = 250;
+            message = `Shipping to ${country} will cost ${price} credits`;
+            break;
+        case 'Australia':
+            price = 170;
+            message = `Shipping to ${country} will cost ${price} credits`;
+            break;
+        case 'Jamaica':
+            price = 120;
+            message = `Shipping to ${country} will cost ${price} credits`;
+            break;
+        default:
+            message = "Sorry, there is no delivery to your country";
+    }
+    return message;
+}
 
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-
-// function getElementWidth(content, padding, border) {
-//     let width = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-//     return width;
-// }
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
