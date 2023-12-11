a607f941fe96d7c8d6b08533d46eb5a93dcd03dd
Dmytro Zl <zl.dmitriy94@gmail.com>
2023-12-11 21:47:07 +0200

    Task 3 finished

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,17 @@
-function getElementWidth(content, padding, border) {
-    let width = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return width;
+function checkForSpam(message) {
+    let toLowerCase = message.toLowerCase();
+    if (toLowerCase.includes("spam") || toLowerCase.includes("sale")) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-
-
-// function getShippingMessage(country, price, deliveryFee) {
-//     let totalPrice = price + deliveryFee;
-//     let message = `Shipping to ${country} will cost ${totalPrice} credits`;
-//     return message
-// }
-
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+// function getShippingMessage(country, price, deliveryFee) {
+//     let totalPrice = price + deliveryFee;
+//     let message = `Shipping to ${country} will cost ${totalPrice} credits`;
+//     return message
+// }
+
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+// function getElementWidth(content, padding, border) {
+//     let width = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+//     return width;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
